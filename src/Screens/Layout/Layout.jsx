c5c1a50d8b797ef7f5d6bb7b60da55a4752c022b
jhantu626 <jhantubala626@gmail.com>
2025-10-15 16:07:39 +0530

    dev: added layout

--- src/Screens/Layout/Layout.jsx
+++ src/Screens/Layout/Layout.jsx
@@ -1,17 +1,17 @@
-import { StyleSheet, Text, View } from 'react-native';
-import { colors } from './../../utils/colors';
+import { StatusBar, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { colors } from '../../utils/colors';
 
 const Layout = ({ children }) => {
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: colors.primaryBackgorund,
-      }}
-    >
+    <SafeAreaView style={{ flex: 1, backgroundColor: colors.screenBackground }}>
+      <StatusBar barStyle={'dark-content'} />
       {children}
-    </View>
+    </SafeAreaView>
   );
 };
 
 export default Layout;
+
+const styles = StyleSheet.create({});
